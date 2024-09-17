b1c3aafdc36b8b2065248e98d066adc66111219d
akarande0906 <alok.karande@gmail.com>
2024-09-17 15:20:20 -0700

    Resolved O(n^2) issues

--- arrays/two_sum/brute_force.py
+++ arrays/two_sum/brute_force.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         for lptr in range (0, len(nums) - 1):
             for  rptr in range(lptr+1, len(nums)):
                 if nums[lptr] + nums[rptr] == target:
@@ -8,5 +8,6 @@ class Solution:
 
 
 if __name__ == '__main__':
-  print(twoSum([2,7,11,15], 9)
-  print(twoSum([3,2,4], 6)
+  print(Solution().twoSum([2,7,11,15], 9))
+  print(Solution().twoSum([3,2,4], 6))
+  print(Solution().twoSum([3,3], 6))
