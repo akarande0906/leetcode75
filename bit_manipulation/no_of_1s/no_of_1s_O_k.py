284fc0b655177048e0942b0bdc94d663c3d01862
akarande0906 <alok.karande@gmail.com>
2024-09-23 00:09:55 -0700

    Solution

---
+++ bit_manipulation/no_of_1s/no_of_1s_O_k.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n > 0:
+            n = n >> 1
+            count += 1
+        return count     
+
+
+sol = Solution()
+print(sol.hammingWeight(10))
+print(sol.hammingWeight(15))
+
